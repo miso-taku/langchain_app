31f0bee31dcce1e9db84e278b6f46cb676435feb
miso-taku <wk.ta93mis@gmail.com>
2024-08-01 21:34:51 +0900

    function calling 3

--- function_calling.py
+++ function_calling.py
@@ -1,12 +1,11 @@
 import json
-from typing import Union
+from typing import Optional
 from operator import itemgetter
 
-from langchain.agents import tool
-from langchain_core.runnables import Runnable, RunnableLambda, RunnablePassthrough
-from langchain.output_parsers import JsonOutputToolsParser
 from langchain_openai import ChatOpenAI
-
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.pydantic_v1 import BaseModel, Field
+from langchain_core.runnables import RunnablePassthrough
 
 def get_openai_apikey() -> str:
     secret_path = "secret.json"
@@ -14,41 +13,24 @@ def get_openai_apikey() -> str:
         secret = json.load(f)
         return secret["OPENAI_API_KEY"]
 
+class Item(BaseModel):
+    item_name: str = Field(description="商品名")
+    price: Optional[int] = Field(description="価格")
+    color: Optional[str] = Field(description="色")
 
+system = "与えられた商品の情報を構造化してください。"
+prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", system),
+        ("human", "{item_info}"),
+    ]
+)
 
 llm = ChatOpenAI(api_key=get_openai_apikey(), model="gpt-4o-mini")
-
-@tool
-def add(first_int: int, second_int: int) -> int:
-    """ Add two integers """
-    return first_int + second_int
-
-@tool
-def exponentiate(base: int, exponent: int) -> int:
-    """ Raise base to the power of exponent """
-    return base ** exponent
-
-@tool
-def multiply(first_int: int, second_int: int) -> int:
-    """ Multiply two integers """
-    return first_int * second_int
-
-tools = [add, exponentiate, multiply]
-tool_map = {tool.name: tool for tool in tools}
-
-def call_tool(tool_invocation: dict) -> Union[str, Runnable]:
-    tool = tool_map[tool_invocation["type"]]
-    return RunnablePassthrough.assign(output=itemgetter("args") | tool)
-
-llm_with_tools = llm.bind_tools([add, exponentiate, multiply])
-
-chain = (llm_with_tools | JsonOutputToolsParser() | RunnableLambda(call_tool).map())
-
-res = chain.invoke("""
-以下の計算をしてください。
-- 123 + 4567
-- 123 * 4567
-- 12**12
-""")
-
+structued_llm = llm.with_structured_output(Item)
+chain = prompt | structued_llm
+res = chain.invoke({"item_info": "林檎　赤　100円"})
+print(res)
+print(res.json(ensure_ascii=False))
+res = chain.invoke({"item_info": "バナナ　80円"})
 print(res)
